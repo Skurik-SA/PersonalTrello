c9ae5718f20483584896bd214884a6ee22471185
Skurik-SA <56246154+Skurik-SA@users.noreply.github.com>
2023-10-07 20:51:42 +1000

    Basic moves

--- src/pages/AuthPages/LoginPage/LoginPage.jsx
+++ src/pages/AuthPages/LoginPage/LoginPage.jsx
@@ -1,9 +1,30 @@
 import styles from "./LoginPage.module.css"
 
 const LoginPage = () => {
+    function removeDuplicates2(arr) {
+        return arr.map(item => {
+            if (!arr.includes(item)) {
+                return item
+            }
+        })
+    }
+
+
+    function removeDuplicates(arr) {
+        const res = []
+        arr.forEach((item, index) => {
+            if(!res.includes(item)) {
+                res.push(item)
+            }
+        })
+        return res
+    }
+
+    const x = [1, 2, 2, 3, 3, 5, 6, 7, 7]
+
     return (
         <>
-            Login
+            Login {removeDuplicates(x)} | {removeDuplicates2(x)}
         </>
     )
 }

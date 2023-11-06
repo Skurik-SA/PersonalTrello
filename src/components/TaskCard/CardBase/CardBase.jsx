ceb13aa60b18cf3daf194260e496c73371323a42
Skurik-SA <56246154+Skurik-SA@users.noreply.github.com>
2023-11-07 06:13:35 +1000

    Added hovered edit pen to TaskBlock in Card

--- src/components/TaskCard/CardBase/CardBase.jsx
+++ src/components/TaskCard/CardBase/CardBase.jsx
@@ -51,9 +51,17 @@ const CardBase = (props) => {
                     {card_data.content.map((task, i) =>
                         <li key={i} className={styles.taskContents}>
                             <div className={styles.taskWrapper}>
-                                <div >
+                                <div className={styles.taskText}>
                                     {task}
                                 </div>
+                                <button className={styles.editTaskButton}>
+                                    <span>
+                                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="0.706004" width="11.0543" height="2.98189" transform="matrix(0.706004 -0.708208 0.706004 0.708208 2.88603 9.03698)" stroke="#DBA498"/>
+                                            <path d="M1.57791 12.417L2.10495 9.14641L4.8383 11.8883L1.57791 12.417Z" fill="#DBA498"/>
+                                        </svg>
+                                    </span>
+                                </button>
                             </div>
                         </li>
                     )
